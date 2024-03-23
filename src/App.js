b74a232637c63a7d22665473db56f6bda57f3629
rosaceaee <corydalisss@gmail.com>
2024-03-23 18:17:44 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import Test from "./Test";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Test.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import dfs_xy_conv from "./xyGrid";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Test = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const url =
+        "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
+      const authKey = API_KEY;
+
+      //get a current geo values
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        //get current lati, long values
+        const lati = position.coords.latitude;
+        const long = position.coords.longitude;
+
+        //convert to match lati and long values in weather KMA api location values
+        const xy = dfs_xy_conv("toXY", parseFloat(lati), parseFloat(long));
+        const xx = `${xy.x}`;
+        const yy = `${xy.y}`;
+        const reqURL = `${url}?serviceKey=${authKey}&numOfRows=10&pageNo=1&dataType=JSON&base_date=20240322&base_time=0630&nx=${xx}&ny=${yy}`;
+        console.log(reqURL);
+
+        try {
+          const response = await fetch(reqURL);
+          const data = await response.json();
+          console.log(data);
+          setWeatherData(data);
+        } catch (error) {
+          console.error("error msg,,", error);
+        }
+      });
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Weather</h1>
+      {weatherData ? (
+        <div>
+          {weatherData.response.body.items.item.map((i, idx) => {
+            return <p key={idx}>{i.baseDate}</p>;
+          })}{" "}
+        </div>
+      ) : (
+        <p>waittttt</p>
+      )}
+    </div>
+  );
+};
+
+export default Test;
